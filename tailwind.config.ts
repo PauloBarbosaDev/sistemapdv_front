a0b8936a37f91016267c06739da5e0b7d88ca0f7
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-06-21 15:04:54 -0300

    chore(tailwind.config.ts): Adicionado cores, sombras e tamanhos de font customizadas

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,35 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        lightBase: 'var(--light-base)',
+        darkBlue: 'var(--dark-blue)',
+        primaryColor: 'var(--primary-color)',
+      },
+      boxShadow: {
+        custom: '0px 3px 10px 2px rgba(20, 42, 63, 0.1)',
+      },
+      fontSize: {
+        title: '3rem',
+        subTitle: '1.5rem',
+        paragraph1: '1.5rem',
+        paragraph2: '1.25rem',
+        paragraph3: '1rem',
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animated')],
 };
 export default config;
