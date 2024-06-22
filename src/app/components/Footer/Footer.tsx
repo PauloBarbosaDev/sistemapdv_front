1d51242a3542a7740b235d70beb08ff5de45083a
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-06-22 00:41:59 -0300

    feat(Footer.tsx): Cria componente Footer

---
+++ src/app/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className="flex items-center justify-center bg-lightBase min-h-2 ">
+        <p>Criado por Paulo Barbosa</p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
