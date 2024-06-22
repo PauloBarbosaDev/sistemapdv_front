4539028f9a48f26b6813e17336e8088fe87cad8a
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-06-22 01:12:12 -0300

    refactor: Arquivos Footer.tsx , Header.tsx , menu.module.scss , Menu.tsx e PresentationSection movidos da pasta components para components/homeNoAuth

---
+++ src/app/components/homeNoAuth/Header/Header.tsx
@@ -0,0 +1,15 @@
+import LogoGeneric from '@/components/Common/LogoGeneric/LogoGeneric';
+import Menu from './Menu/Menu';
+
+const Header = () => {
+  return (
+    <>
+      <header className="bg-lightBase flex items-center justify-between px-24 min-h-20 shadow-custom">
+        <LogoGeneric />
+        <Menu />
+      </header>
+    </>
+  );
+};
+
+export default Header;
