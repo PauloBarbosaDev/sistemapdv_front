50cfa0ff776cb449bfec9835080579f7a47dffa2
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-06-22 02:28:46 -0300

    feat: adicionar ícone de seta no componente
    - Incluído ícone `iconArrowDown` em `Image` no componente
    - Posicionado centralmente usando classes Flexbox

--- src/app/components/homeNoAuth/PresentationSection/PresentationSection.tsx
+++ src/app/components/homeNoAuth/PresentationSection/PresentationSection.tsx
@@ -1,12 +1,23 @@
+import Image from 'next/image';
 import PresentationImage from './PresentationImage/PresentationImage';
 import SectionContent from './SectionContent/SectionContent';
 
 const PresentationSection = () => {
   return (
-    <div className="flex justify-around items-center ">
-      <SectionContent />
-      <PresentationImage />
-    </div>
+    <>
+      <div className="flex justify-around items-center ">
+        <SectionContent />
+        <PresentationImage />
+      </div>
+      <div className="flex justify-center items-center">
+        <Image
+          src={'/homeNoAuth/iconArrowDown.svg'}
+          alt={'arrowDown'}
+          width={46.66}
+          height={25.08}
+        />
+      </div>
+    </>
   );
 };
 
