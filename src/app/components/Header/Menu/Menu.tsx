86123eafffddef8a8494351febda2c9922415447
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-06-22 00:43:25 -0300

    feat(Menu.tsx): Cria componente Menu

---
+++ src/app/components/Header/Menu/Menu.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import { FiUser } from 'react-icons/fi';
+import styles from './menu.module.scss';
+import ButtomGeneric from '@/components/Common/ButtomGeneric/ButtomGeneric';
+
+const Menu = () => {
+  return (
+    <nav className="menu flex gap-10 items-center">
+      <Link href="/" className={styles.link}>
+        Início
+      </Link>
+      <Link href="/about" className={styles.link}>
+        Sobre
+      </Link>
+
+      <ButtomGeneric
+        target="/login"
+        content="Acessar"
+        width={8.75}
+        height={2.875}
+        color="var(--dark-blue)"
+        bgColor="var(--light-base)"
+        bdColor="var(--primary-color)"
+        icon={FiUser}
+      />
+
+      <ButtomGeneric
+        target="/register"
+        content="Teste grátis"
+        width={8.75}
+        height={2.875}
+        color="var(--white-color)"
+        bgColor="var(--primary-color)"
+      />
+    </nav>
+  );
+};
+
+export default Menu;
