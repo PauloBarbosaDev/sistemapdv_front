45b817ed8e72f9c71beaee6dfea70cb2cf9d294b
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-06-25 03:31:09 -0300

    refactor(PresentationImage.tsx): substituir section por div para melhorar a semântica

--- src/app/components/homeNoAuth/PresentationSection/PresentationImage/PresentationImage.tsx
+++ src/app/components/homeNoAuth/PresentationSection/PresentationImage/PresentationImage.tsx
@@ -2,14 +2,14 @@ import Image from 'next/image';
 
 const PresentationImage = () => {
   return (
-    <section>
+    <div>
       <Image
         src={'homeNoAuth/imgPresentation.svg'}
         alt={'Imagem de apresentação'}
         width={493.18}
         height={486}
       ></Image>
-    </section>
+    </div>
   );
 };
 
