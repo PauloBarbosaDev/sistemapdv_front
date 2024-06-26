f0ffd8137f05231aed08a79dd7ca64097aeda534
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-06-26 13:07:11 -0300

    feat(FooterBottom.tsx): cria componente de copyRight

---
+++ src/app/components/homeNoAuth/Footer/FooterBottom/FooterBottom.tsx
@@ -0,0 +1,14 @@
+const FooterBottom = () => {
+  return (
+    <div className="bg-lightBase pb-5">
+      <p className="text-center font-semibold text-darkBlue text-sm">
+        Website created By{' '}
+        <span className="text-primaryColor font-bold text-sm">
+          Paulo Barbosa
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default FooterBottom;
