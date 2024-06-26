aab5e0e3f8da54dd66c3ad037105c596361ff6fc
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-06-26 13:04:46 -0300

    feat(HelpCards.tsx): cria componente HelpCards organizar os cards da HelpSection

---
+++ src/app/components/homeNoAuth/HelpSection/HelpCards/HelpCards.tsx
@@ -0,0 +1,62 @@
+import CardGeneric from '../../../Common/CardGeneric/CardGeneric';
+
+const HelpCards = () => {
+  return (
+    <div className=" w-[58.75rem] h-[33rem] flex flex-wrap items-center justify-center gap-[3.25rem]">
+      <CardGeneric
+        imageLink="/homeNoAuth/icons8-marcador.svg"
+        imageAlt="icone-marcador"
+        title="LOCALIZAÇÃO"
+        description="Rua exemplo no site n:400 Alagoas"
+        width={27.75}
+        height={15.125}
+        imageClasses="p-t[1.875rem] pb-8"
+        titleClasses="text-paragraph1 font-bold text-darkBlue pb-[0.625rem]"
+        descriptionClasses="text-paragraph2 text-darkBlue"
+      />
+
+      <CardGeneric
+        imageLink="/homeNoAuth/icons8-telefone.svg"
+        imageAlt="icone-telefone"
+        title="TELEFONE"
+        description="Whatsapp: (82)99402-1172"
+        width={27.75}
+        height={15.125}
+        imageClasses="p-t[2.313rem] pb-[1.375rem]"
+        titleClasses="text-paragraph1 font-bold text-darkBlue pb-[0.625rem]"
+        descriptionClasses="text-paragraph2 text-darkBlue"
+      />
+
+      <CardGeneric
+        imageLink="/homeNoAuth/icons8-perguntas-frequentes.svg"
+        imageAlt="icone-faq"
+        title="FAQ"
+        description={
+          <span>
+            Clique <span className="text-primaryColor">aqui</span> para acessar
+            nosso faq
+          </span>
+        }
+        width={27.75}
+        height={15.125}
+        imageClasses="p-t[2.125rem] pb-[1.063rem]"
+        titleClasses="text-paragraph1 font-bold text-darkBlue pb-[0.625rem]"
+        descriptionClasses="text-paragraph2 text-darkBlue"
+      />
+
+      <CardGeneric
+        imageLink="/homeNoAuth/icons8-nova-mensagem.svg"
+        imageAlt="icone-email"
+        title="EMAIL"
+        description="pauloabarbosa.dev@gmail.com"
+        width={27.75}
+        height={15.125}
+        imageClasses="p-t[2.125rem] pb-[1.375rem]"
+        titleClasses="text-paragraph1 font-bold text-darkBlue pb-[0.625rem]"
+        descriptionClasses="text-paragraph2 text-darkBlue"
+      />
+    </div>
+  );
+};
+
+export default HelpCards;
