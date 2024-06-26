99f329e8c61bfd01e7174820ff072e0737a23322
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-06-26 11:33:34 -0300

    feat(Footer.tsx): adiciona div de Copyright

--- src/app/components/homeNoAuth/Footer/Footer.tsx
+++ src/app/components/homeNoAuth/Footer/Footer.tsx
@@ -38,6 +38,14 @@ const Footer = () => {
           />
         </div>
       </footer>
+      <div className="bg-lightBase pb-5">
+        <p className="text-center font-semibold text-darkBlue text-sm">
+          Website created By{' '}
+          <span className="text-primaryColor font-bold text-sm">
+            Paulo Barbosa
+          </span>
+        </p>
+      </div>
     </>
   );
 };
