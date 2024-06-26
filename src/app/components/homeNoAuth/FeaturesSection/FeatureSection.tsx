d1980f3574dcedf61b2a2560bdc92674095b5736
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-06-26 13:11:40 -0300

    refactor(FeatureSection.tsx): dividir componente FeatureSection em TitleGenecic e FeaturesCards

--- src/app/components/homeNoAuth/FeaturesSection/FeatureSection.tsx
+++ src/app/components/homeNoAuth/FeaturesSection/FeatureSection.tsx
@@ -1,65 +1,16 @@
-import CardGeneric from '../../Common/CardGeneric/CardGeneric';
+import TitleGeneric from '../../Common/TitleGeneric/TitleGeneric';
+import FeaturesCards from './FeaturesCards/FeaturesCards';
 
 const FeatureSection = () => {
   return (
     <>
       <section className="flex flex-col p-[12.5rem] items-center justify-center gap-28">
-        <p className="text-title font-bold text-darkBlue w-[48.938rem] text-center leading-[3.5rem]">
-          Descubra como o PSistemaPDV pode impulsionar o seu negócio!
-        </p>
-        <div className=" text-center flex items-center justify-center gap-11 ">
-          <CardGeneric
-            imageLink="/homeNoAuth/icons8-em-alta.svg"
-            imageAlt="icone-em-alta"
-            title="ESTOQUE"
-            description="Gerencie o estoque em tempo real para manter a loja sempre
-              abastecida, organizada e eficiente."
-            width={19.813}
-            height={25.5}
-            imageClasses="p-t[3.25rem] pb-10"
-            titleClasses="text-paragraph1 font-bold text-darkBlue pb-5"
-            descriptionClasses="text-paragraph2 text-darkBlue w-[16.272rem]"
-          />
-
-          <CardGeneric
-            imageLink="/homeNoAuth/icons8-passageiros-com-bagagem.svg"
-            imageAlt="icone-passageiros-com-bagagem"
-            title="CHECKOUT"
-            description="Facilite o checkout para uma experiência de compra rápida,
-            intuitiva e convenientemente eficaz."
-            width={19.813}
-            height={25.5}
-            imageClasses="p-t[3.25rem] pb-10"
-            titleClasses="text-paragraph1 font-bold text-darkBlue pb-5"
-            descriptionClasses="text-paragraph2 text-darkBlue w-[16.272rem]"
-          />
-
-          <CardGeneric
-            imageLink="/homeNoAuth/icons8-seguranca-verificada.svg"
-            imageAlt="icone-seguranca-verificada"
-            title="SEGURANÇA"
-            description="Proteja transações com segurança avançada de pagamento e
-            confiabilidade excepcional."
-            width={19.813}
-            height={25.5}
-            imageClasses="p-t[3.25rem] pb-10"
-            titleClasses="text-paragraph1 font-bold text-darkBlue pb-5"
-            descriptionClasses="text-paragraph2 text-darkBlue w-[16.272rem]"
-          />
-
-          <CardGeneric
-            imageLink="/homeNoAuth/icons8-assistente.svg"
-            imageAlt="icone-assistente"
-            title="SUPORTE"
-            description=" Oferecemos suporte e atendimento para garantir sua tranquilidade e
-            satisfação contínua."
-            width={19.813}
-            height={25.5}
-            imageClasses="p-t[3.25rem] pb-10"
-            titleClasses="text-paragraph1 font-bold text-darkBlue pb-5"
-            descriptionClasses="text-paragraph2 text-darkBlue w-[16.272rem]"
-          />
-        </div>
+        <TitleGeneric
+          content="Descubra como o PSistemaPDV pode impulsionar o seu negócio!"
+          color="var(--dark-blue)"
+          witdh="48.938"
+        />
+        <FeaturesCards />
       </section>
     </>
   );
