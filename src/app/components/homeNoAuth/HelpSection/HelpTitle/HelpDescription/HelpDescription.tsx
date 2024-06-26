2e9ed31ad3d5606d653777fcc7b9b0bb57221535
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-06-26 13:01:12 -0300

    feat(HelpDescription.tsx): cria componente HelpDescription para a descrição do HelpTitle

---
+++ src/app/components/homeNoAuth/HelpSection/HelpTitle/HelpDescription/HelpDescription.tsx
@@ -0,0 +1,16 @@
+const HelpDescription = () => {
+  return (
+    <p className="w-[37.25rem] text-darkBlue">
+      <span className="font-bold">
+        {' '}
+        Nosso suporte está disponível 24 horas por dia para oferecer assistência
+        excepcional a você.
+      </span>{' '}
+      Com uma equipe dedicada, estamos sempre prontos para atender às suas
+      necessidades, garantindo uma experiência de suporte de alta qualidade a
+      qualquer momento do dia ou da noite.
+    </p>
+  );
+};
+
+export default HelpDescription;
