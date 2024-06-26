f224b4128569a167f41b202b0d756fbe9d80e36c
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-06-26 12:56:24 -0300

    chore(DemoSectionTitle.tsx): componente DemoSectionTitle.tsx

--- src/app/components/homeNoAuth/DemoSection/DemoSectionTitle/DemoSectionTitle.tsx
+++
@@ -1,9 +0,0 @@
-const DemoSectionTitle = () => {
-  return (
-    <p className="text-title font-bold w-[54.875rem] leading-[3.5rem]">
-      Transforme seu Negócio com Nossa Avançada Solução PDV Integrada
-    </p>
-  );
-};
-
-export default DemoSectionTitle;
