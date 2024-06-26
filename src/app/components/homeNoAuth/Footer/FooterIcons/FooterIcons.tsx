ed6f3624464adf5cbfb8fbe7adf59b3b33bc7030
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-06-26 11:35:32 -0300

    refactor(FooterIcons.tsx): adiciona o atributo className com animação no hover

--- src/app/components/homeNoAuth/Footer/FooterIcons/FooterIcons.tsx
+++ src/app/components/homeNoAuth/Footer/FooterIcons/FooterIcons.tsx
@@ -4,34 +4,39 @@ const FooterIcons = () => {
   return (
     <div className="flex gap-[1.875rem]">
       <Image
-        src={'/homeNoAuth/Footer/instagram.svg'}
+        src="/homeNoAuth/Footer/instagram.svg"
         alt="icon-instagram"
         width={40}
         height={40}
+        className="transition-transform duration-300 ease-in-out hover:scale-110"
       />
       <Image
-        src={'/homeNoAuth/Footer/linkedin.svg'}
+        src="/homeNoAuth/Footer/linkedin.svg"
         alt="icon-linkedin"
         width={40}
         height={40}
+        className="transition-transform duration-300 ease-in-out hover:scale-110"
       />
       <Image
-        src={'/homeNoAuth/Footer/youtube.svg'}
+        src="/homeNoAuth/Footer/youtube.svg"
         alt="icon-youtube"
         width={40}
         height={40}
+        className="transition-transform duration-300 ease-in-out hover:scale-110"
       />
       <Image
-        src={'/homeNoAuth/Footer/twitter.svg'}
+        src="/homeNoAuth/Footer/twitter.svg"
         alt="icon-twitter"
         width={40}
         height={40}
+        className="transition-transform duration-300 ease-in-out hover:scale-110"
       />
       <Image
-        src={'/homeNoAuth/Footer/facebook.svg'}
+        src="/homeNoAuth/Footer/facebook.svg"
         alt="icon-facebook"
         width={40}
         height={40}
+        className="transition-transform duration-300 ease-in-out hover:scale-110"
       />
     </div>
   );
