a37894204a98108444db281bdcc68a9bdb318198
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-07-08 13:44:02 -0300

    style(Header.tsx): ajusta classes CSS para responsividade

--- src/app/components/homeNoAuth/Header/Header.tsx
+++ src/app/components/homeNoAuth/Header/Header.tsx
@@ -4,7 +4,7 @@ import Menu from './Menu/Menu';
 const Header = () => {
   return (
     <>
-      <header className="bg-lightBase flex items-center justify-between px-24 min-h-20 shadow-custom">
+      <header className="bg-lightBase flex items-center justify-center md:justify-between px-8 min-h-20 shadow-custom sticky top-0 z-20">
         <LogoGeneric />
         <Menu />
       </header>
