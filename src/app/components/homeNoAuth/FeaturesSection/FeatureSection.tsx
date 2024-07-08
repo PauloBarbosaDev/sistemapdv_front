4798000c062dea0cf6ee33a34bff715fb495bfb9
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-07-08 13:33:38 -0300

    style(FeatureSection.tsx): remove o atributo de tamanho e adiciona classes de responsividade

--- src/app/components/homeNoAuth/FeaturesSection/FeatureSection.tsx
+++ src/app/components/homeNoAuth/FeaturesSection/FeatureSection.tsx
@@ -4,11 +4,10 @@ import FeaturesCards from './FeaturesCards/FeaturesCards';
 const FeatureSection = () => {
   return (
     <>
-      <section className="flex flex-col p-[12.5rem] items-center justify-center gap-28">
+      <section className="flex flex-col p-14 md:p-44 lg:p-52 items-center justify-center gap-28">
         <TitleGeneric
           content="Descubra como o PSistemaPDV pode impulsionar o seu negócio!"
           color="var(--dark-blue)"
-          witdh="48.938"
         />
         <FeaturesCards />
       </section>
