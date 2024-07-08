0816c121356eda357b213ed3f8073c4af02ca194
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-07-08 13:30:04 -0300

    style(TitleGeneric.tsx): remove a props witdh e adiciona classes de responsividade

--- src/app/components/Common/TitleGeneric/TitleGeneric.tsx
+++ src/app/components/Common/TitleGeneric/TitleGeneric.tsx
@@ -1,15 +1,13 @@
 interface props {
   color: string;
-  witdh: string;
   content: string;
 }
 
-const TitleGeneric = ({ color, witdh, content }: props) => {
+const TitleGeneric = ({ color, content }: props) => {
   return (
     <p
-      className="text-title font-bold text-center leading-[3.5rem]"
+      className="text-4xl max-w-sm sm:text-5xl sm:max-w-4xl text-center font-bold text-darkBlue leading-[2.4rem] sm:leading-[3.563rem]"
       style={{
-        width: `${witdh}rem`,
         color: `${color}`,
       }}
     >
