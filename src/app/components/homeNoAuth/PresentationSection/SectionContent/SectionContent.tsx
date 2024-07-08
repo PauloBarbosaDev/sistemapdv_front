35c0e223bf61dddfdc4aeacd55c7ff498aab1319
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-07-08 13:48:58 -0300

    style(SectionContent.tsx): ajusta classes CSS para responsividade

--- src/app/components/homeNoAuth/PresentationSection/SectionContent/SectionContent.tsx
+++ src/app/components/homeNoAuth/PresentationSection/SectionContent/SectionContent.tsx
@@ -4,12 +4,12 @@ import { FiArrowRight } from 'react-icons/fi';
 const SectionContent = () => {
   return (
     <div>
-      <div className="flex flex-col p-4 gap-9">
-        <p className="text-title w-[39rem] text-left font-bold text-darkBlue leading-[3.563rem]">
+      <div className="flex flex-col p-2 md:p-4 gap-8 md:gap-6 lg:gap-9">
+        <p className="text-4xl max-w-sm sm:text-5xl sm:max-w-2xl text-left font-bold text-darkBlue leading-[2.4rem] sm:leading-[3.563rem]">
           Potencialize sua experiência de compra com nosso novo{' '}
           <span className="text-primaryColor">sistema PDV integrado</span>!
         </p>
-        <p className="text-paragraph1 w-[41.208rem] text-darkBlue">
+        <p className="text-lg max-w-md sm:text-paragraph2 sm:max-w-2xl text-darkBlue">
           Simplifique suas operações de checkout, controle seu estoque em tempo
           real e impulsione o crescimento do seu negócio com nosso sistema PDV
           intuitivo e confiável.
@@ -19,22 +19,18 @@ const SectionContent = () => {
         <ButtomGeneric
           target="/register"
           content="Teste grátis"
-          width={12.313}
-          height={3.688}
           color="var(--white-color)"
           bgColor="var(--primary-color)"
-          ftSize={1.25}
+          ftSize={1.125}
         />
 
         <ButtomGeneric
           target="/register"
           content="Saiba mais"
-          width={12.813}
-          height={3.688}
           color="var(--dark-blue)"
           bgColor="var(--light-base)"
           bdColor="var(--primary-color)"
-          ftSize={1.25}
+          ftSize={1.125}
           iconPosition="after"
           icon={FiArrowRight}
         />
