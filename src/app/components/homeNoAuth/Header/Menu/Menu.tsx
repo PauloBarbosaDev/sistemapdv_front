c0713ec5459259ccfb3b1397d1b4893339f24c54
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-07-08 13:44:46 -0300

    style(Menu.tsx): ajusta classes CSS para responsividade e adiciona animação ao hover

--- src/app/components/homeNoAuth/Header/Menu/Menu.tsx
+++ src/app/components/homeNoAuth/Header/Menu/Menu.tsx
@@ -1,23 +1,23 @@
 import Link from 'next/link';
 import { FiUser } from 'react-icons/fi';
-import styles from './menu.module.scss';
 import ButtomGeneric from '@/app/components/Common/ButtonGeneric/ButtonGeneric';
 
 const Menu = () => {
   return (
-    <nav className="menu flex gap-10 items-center">
-      <Link href="/" className={styles.link}>
+    <nav className="gap-10 items-center hidden md:flex text-base">
+      <Link href="/" className={'text-darkBlue font-bold hover:animate-pulse'}>
         Início
       </Link>
-      <Link href="/about" className={styles.link}>
+      <Link
+        href="/about"
+        className={' text-darkBlue font-bold hover:animate-pulse'}
+      >
         Sobre
       </Link>
 
       <ButtomGeneric
         target="/login"
         content="Acessar"
-        width={8.75}
-        height={2.875}
         color="var(--dark-blue)"
         bgColor="var(--light-base)"
         bdColor="var(--primary-color)"
@@ -27,8 +27,6 @@ const Menu = () => {
       <ButtomGeneric
         target="/register"
         content="Teste grátis"
-        width={8.75}
-        height={2.875}
         color="var(--white-color)"
         bgColor="var(--primary-color)"
       />
