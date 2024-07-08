ac38017d569b5c749f423cd18e1bd3485781e7fb
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-07-08 13:31:17 -0300

    style(DemoSection.tsx): remove o atributo de tamanho e adiciona classes de responsividade

--- src/app/components/homeNoAuth/DemoSection/DemoSection.tsx
+++ src/app/components/homeNoAuth/DemoSection/DemoSection.tsx
@@ -4,15 +4,14 @@ import SectionContent from './SectionContent/SectionContent';
 
 const DemoSection = () => {
   return (
-    <section className="flex flex-col p-16 bg-darkBlue text-white">
+    <section className="flex flex-col p-14 bg-darkBlue text-white">
       <div className="flex items-center justify-center">
         <TitleGeneric
           content="Transforme seu Negócio com Nossa Avançada Solução PDV Integrada"
           color="var(--white-color)"
-          witdh="54.875"
         />
       </div>
-      <div className="flex justify-around items-center pt-[6.25rem]">
+      <div className="flex justify-around items-center pt-[6.25rem] flex-wrap-reverse gap-9">
         <DemoVideo />
         <SectionContent />
       </div>
