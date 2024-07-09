b9b941fa7c7b01499479f8561b4714250894ce68
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-07-09 02:30:05 -0300

    style(HelpCards.tsx): ajustar classes de estilo responsivo no componente HelpCards
    - Ajustadas classes de espaçamento e tamanho de texto para melhorar a responsividade em diferentes tamanhos de tela
    - Mantido o layout e a estrutura do componente

--- src/app/components/homeNoAuth/HelpSection/HelpCards/HelpCards.tsx
+++ src/app/components/homeNoAuth/HelpSection/HelpCards/HelpCards.tsx
@@ -8,11 +8,10 @@ const HelpCards = () => {
         imageAlt="icone-marcador"
         title="LOCALIZAÇÃO"
         description="Rua exemplo no site n:400 Alagoas"
-        width={27.75}
-        height={15.125}
-        imageClasses="p-t[1.875rem] pb-8"
-        titleClasses="text-paragraph1 font-bold text-darkBlue pb-[0.625rem]"
-        descriptionClasses="text-paragraph2 text-darkBlue"
+        imageClasses="pt-[1.875rem] pb-8"
+        titleClasses="text-paragraph2 sm:text-paragraph1 font-bold text-darkBlue pb-[0.625rem]"
+        descriptionClasses="text-base sm:text-paragraph2 text-darkBlue"
+        containerClasses="w-full sm:w-[27.75rem] h-[15.125rem]"
       />
 
       <CardGeneric
@@ -20,11 +19,10 @@ const HelpCards = () => {
         imageAlt="icone-telefone"
         title="TELEFONE"
         description="Whatsapp: (82)99402-1172"
-        width={27.75}
-        height={15.125}
-        imageClasses="p-t[2.313rem] pb-[1.375rem]"
-        titleClasses="text-paragraph1 font-bold text-darkBlue pb-[0.625rem]"
-        descriptionClasses="text-paragraph2 text-darkBlue"
+        imageClasses="pt-[2.313rem] pb-[1.375rem]"
+        titleClasses="text-paragraph2 sm:text-paragraph1 font-bold text-darkBlue pb-[0.625rem]"
+        descriptionClasses="text-base sm:text-paragraph2 text-darkBlue"
+        containerClasses="w-full sm:w-[27.75rem] h-[15.125rem]"
       />
 
       <CardGeneric
@@ -37,11 +35,10 @@ const HelpCards = () => {
             nosso faq
           </span>
         }
-        width={27.75}
-        height={15.125}
-        imageClasses="p-t[2.125rem] pb-[1.063rem]"
-        titleClasses="text-paragraph1 font-bold text-darkBlue pb-[0.625rem]"
-        descriptionClasses="text-paragraph2 text-darkBlue"
+        imageClasses="pt-[2.125rem] pb-[1.063rem]"
+        titleClasses="text-paragraph2 sm:text-paragraph1 font-bold text-darkBlue pb-[0.625rem]"
+        descriptionClasses="text-base sm:text-paragraph2 text-darkBlue"
+        containerClasses="w-full sm:w-[27.75rem] h-[15.125rem]"
       />
 
       <CardGeneric
@@ -49,11 +46,10 @@ const HelpCards = () => {
         imageAlt="icone-email"
         title="EMAIL"
         description="pauloabarbosa.dev@gmail.com"
-        width={27.75}
-        height={15.125}
-        imageClasses="p-t[2.125rem] pb-[1.375rem]"
-        titleClasses="text-paragraph1 font-bold text-darkBlue pb-[0.625rem]"
-        descriptionClasses="text-paragraph2 text-darkBlue"
+        imageClasses="pt-[2.125rem] pb-[1.375rem]"
+        titleClasses="text-paragraph2 sm:text-paragraph1 font-bold text-darkBlue pb-[0.625rem]"
+        descriptionClasses="text-base sm:text-paragraph2 text-darkBlue"
+        containerClasses="w-full sm:w-[27.75rem] h-[15.125rem]"
       />
     </div>
   );
