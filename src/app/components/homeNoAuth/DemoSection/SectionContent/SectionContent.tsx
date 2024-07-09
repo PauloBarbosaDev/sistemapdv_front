cc5d6b099d7e85d6b5e91d244e256682e0b40e26
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-07-09 02:12:11 -0300

    style(SectionContent.tsx): esconde botão e texto com hidden e sm:block para retornar somente em resolução mais alta

--- src/app/components/homeNoAuth/DemoSection/SectionContent/SectionContent.tsx
+++ src/app/components/homeNoAuth/DemoSection/SectionContent/SectionContent.tsx
@@ -4,17 +4,17 @@ const SectionContent = () => {
   return (
     <div>
       <div className="flex flex-col p-4 gap-9">
-        <p className="text-3xl max-w-xs sm:text-4xl sm:max-w-2xl text-left font-bold md:leading-tight ">
+        <p className="hidden sm:block text-3xl max-w-xs sm:text-4xl sm:max-w-2xl text-center sm:text-left font-bold sm:leading-[3.563rem] md:leading-tight ">
           Assista ao vídeo e descubra como nosso novo Sistema PDV aprimora sua
           experiência de compra!
         </p>
-        <p className="text-xl max-w-[40.625rem] leading-7 text-left">
+        <p className="text-xl max-w-[40.625rem] leading-7 text-center sm:text-left">
           Descubra nosso sistema PDV em ação! Assista a uma demonstração
           envolvente que destaca os recursos essenciais para impulsionar seu
           negócio. Veja como nossa solução simplifica suas operações de vendas.
         </p>
       </div>
-      <div className="">
+      <div className="hidden sm:block">
         <ButtomGeneric
           target="/login"
           content="TESTE GRÁTIS"
