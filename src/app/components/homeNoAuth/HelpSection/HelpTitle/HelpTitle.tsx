5fc69b1c10addabb3815a292ff7ec1a4331ac872
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-07-09 02:31:00 -0300

    style(HelpTitle.tsx): ajustar largura máxima do título no componente HelpTitle
    - Ajustado o max-width do título para melhorar a responsividade em telas maiores
    - Mantido o layout e a estrutura do componente

--- src/app/components/homeNoAuth/HelpSection/HelpTitle/HelpTitle.tsx
+++ src/app/components/homeNoAuth/HelpSection/HelpTitle/HelpTitle.tsx
@@ -3,7 +3,7 @@ import HelpDescription from './HelpDescription/HelpDescription';
 const HelpTitle = () => {
   return (
     <div className="flex flex-col gap-9 text-center">
-      <p className="text-4xl sm:text-5xl  font-bold text-darkBlue ">
+      <p className="text-4xl max-w-sm sm:text-5xl sm:max-w-4xl  font-bold text-darkBlue ">
         Precisa de ajuda?
       </p>
       <HelpDescription />
