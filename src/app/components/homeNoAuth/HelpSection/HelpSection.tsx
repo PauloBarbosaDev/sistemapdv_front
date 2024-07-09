1a3ae143a97801f408a512a0e5ae558b65d2ebd7
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-07-09 02:29:02 -0300

    style(HelpSection.tsx): ajustar padding no componente HelpSection
    - Alterado o padding de p-14 para p-16 para melhorar a aparência e o espaçamento

--- src/app/components/homeNoAuth/HelpSection/HelpSection.tsx
+++ src/app/components/homeNoAuth/HelpSection/HelpSection.tsx
@@ -3,7 +3,7 @@ import HelpTitle from './HelpTitle/HelpTitle';
 
 const HelpSection = () => {
   return (
-    <section className="flex flex-col p-14 md:p-44 lg:p-52 items-center justify-center gap-28">
+    <section className="flex flex-col p-16 md:p-44 lg:p-52 items-center justify-center gap-28">
       <HelpTitle />
       <HelpCards />
     </section>
