a974b0cc8db1d9a0f228aa40dc7c52811dc88c5c
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-07-10 04:47:55 -0300

    feat(authService.ts): adicionar serviço de autenticação com função de login utilizando api.ts

---
+++ src/app/services/authService.ts
@@ -0,0 +1,26 @@
+import api from './api';
+
+interface LoginParams {
+  email: string;
+  password: string;
+}
+
+const authService = {
+  login: async (params: LoginParams) => {
+    const res = await api.post('/auth/login', params).catch(error => {
+      if (error.response.status === 400 || error.response.status === 401) {
+        return error.response;
+      }
+
+      return error;
+    });
+
+    if (res.status === 200) {
+      sessionStorage.setItem('sistemaPDV-token', res.data.token);
+    }
+
+    return res;
+  },
+};
+
+export default authService;
