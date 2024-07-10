b01d625c6fd8c85209c8807089dd40d3d6d09729
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-07-10 01:45:47 -0300

    style(FooterMain.tsx): ajustar padding e largura máxima

--- src/app/components/homeNoAuth/Footer/FooterMain/FooterMain.tsx
+++ src/app/components/homeNoAuth/Footer/FooterMain/FooterMain.tsx
@@ -4,15 +4,15 @@ import FooterList from '../FooterList/FooterList';
 
 const FooterMain = () => {
   return (
-    <div className="flex justify-around items-center bg-lightBase min-h-[26.313rem]">
+    <div className="flex justify-around items-center bg-lightBase min-h-[26.313rem] lg:p-8 xl:p-0">
       <div className="flex flex-col gap-8 p-3 sm:p-0">
         <LogoGeneric />
-        <p className="text-4xl max-w-sm sm:text-5xl sm:max-w-lg leading-[2.4rem] sm:leading-[3.563rem] text-darkBlue  font-bold">
+        <p className="text-4xl max-w-sm sm:text-5xl sm:max-w-lg lg:max-w-sm xl:max-w-lg leading-[2.4rem] sm:leading-[3.563rem] text-darkBlue  font-bold">
           Vamos transformar seu negócio?
         </p>
         <FooterIcons />
       </div>
-      <div className="hidden lg:flex lg:gap-[6.25rem]">
+      <div className="hidden lg:flex lg:gap-10 xl:flex xl:gap-[6.25rem]">
         <FooterList
           title="PsistemaPDV"
           items={[
