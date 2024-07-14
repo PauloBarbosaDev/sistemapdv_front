e054cfee7b0961fd142294f05ea52a39fbae9a9f
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-07-14 04:24:28 -0300

    feat(authService.ts): adicionar funcionalidade de registro de usuário ao authService

--- src/app/services/authService.ts
+++ src/app/services/authService.ts
@@ -1,11 +1,29 @@
 import api from './api';
 
+interface RegisterParams {
+  name: string;
+  email: string;
+  password: string;
+}
+
 interface LoginParams {
   email: string;
   password: string;
 }
 
 const authService = {
+  register: async (params: RegisterParams) => {
+    const res = await api.post('/auth/register', params).catch(error => {
+      if (error.response.status === 400) {
+        return error.response;
+      }
+
+      return error;
+    });
+
+    return res;
+  },
+
   login: async (params: LoginParams) => {
     const res = await api.post('/auth/login', params).catch(error => {
       if (error.response.status === 400 || error.response.status === 401) {
