5339d67cfc39a026adb50daf69d818e6400708a2
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-07-15 04:18:05 -0300

    feat(dov.env): adiciona e configura url por dotenv

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 
-const baseURL: string | undefined = 'http://localhost:3000';
+const baseURL: string | undefined = process.env.NEXT_PUBLIC_BASEURL;
 
 const api: AxiosInstance = axios.create({
   baseURL,
