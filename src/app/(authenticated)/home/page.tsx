22bb2854b87b8d0ed840fc708d9ae4fbc0424953
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-09-25 13:00:07 -0300

    feat: criada página e layout  autenticada

---
+++ src/app/(authenticated)/home/page.tsx
@@ -0,0 +1,11 @@
+import HomeContent from '@/app/components/homeAuth/HomeContent/HomeContent';
+
+const Home = () => {
+  return (
+    <>
+      <HomeContent />
+    </>
+  );
+};
+
+export default Home;
