d12963adefb5b8bf30e507b051c92b3319e3f29e
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-09-25 13:05:44 -0300

    feat: cria um loading para ser exibido no lugar dos dados caso não houver

---
+++ src/app/components/Common/LoadingGeneric/LoadingGeneric.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="h-screen w-full d-flex justify-center align-items-center">
+      <p className="text-2xl text-yellow-500">Carregando...</p>
+    </div>
+  );
+};
+
+export default Loading;
