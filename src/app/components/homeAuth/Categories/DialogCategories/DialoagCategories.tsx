a2cf0a65aa0f1c73eebed42e643bd008e5262375
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-09-30 20:42:37 -0300

    feat: adicionar suporte a filtragem por data no componente de categorias

--- src/app/components/homeAuth/Categories/DialogCategories/DialoagCategories.tsx
+++ src/app/components/homeAuth/Categories/DialogCategories/DialoagCategories.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   Dialog,
   DialogTrigger,
@@ -9,9 +9,10 @@ import {
 import { DialogHeader, DialogFooter } from '../../../ui/dialog';
 import { CategoryType } from '@/app/services/categoryService';
 import { LucideEdit } from 'lucide-react';
+import { Switch } from '@/app/components/ui/switch';
 
 interface Props {
-  type: 'NOVA' | 'EDITAR';
+  type: 'NOVA' | 'EDITAR' | 'FILTRAR';
   buttonCaption?: string;
   editDescription: string;
   onClick: (category: CategoryType) => void;
@@ -24,14 +25,44 @@ export function DialogCategories({
   type,
 }: Props) {
   const [description, setDescription] = useState(editDescription);
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [isDisabledStartDate, setIsDisabledStartDate] = useState(true);
+  const [isDisabledEndDate, setIsDisabledEndDate] = useState(true);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setDescription(e.target.value);
   };
 
+  const handleStartDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStartDate(e.target.value);
+  };
+
+  const handleEndDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEndDate(e.target.value);
+  };
+
+  const toggleDateFilter = (checked: boolean) => {
+    if (checked) {
+      setIsDisabledStartDate(false);
+      setIsDisabledEndDate(false);
+    } else {
+      setIsDisabledStartDate(true);
+      setIsDisabledEndDate(true);
+      setStartDate('');
+      setEndDate('');
+    }
+  };
+
   const handleSaveClick = () => {
-    onClick({ description });
-    setDescription('');
+    if (type === 'FILTRAR') {
+      onClick({
+        createdAt: [startDate, endDate],
+        description,
+      });
+    } else {
+      onClick({ description });
+    }
   };
 
   return (
@@ -40,10 +71,20 @@ export function DialogCategories({
         <button
           type="button"
           className={`p-3 ${
-            type === 'EDITAR' ? 'text-black' : 'bg-green-400  text-white'
+            type === 'FILTRAR'
+              ? 'bg-yellow-400 text-white'
+              : type === 'EDITAR'
+              ? 'text-black'
+              : 'bg-green-400 text-white'
           } flex items-center`}
         >
-          {type === 'EDITAR' ? <LucideEdit /> : buttonCaption}
+          {type === 'EDITAR' ? (
+            <LucideEdit />
+          ) : type === 'FILTRAR' ? (
+            buttonCaption
+          ) : (
+            buttonCaption
+          )}
         </button>
       </DialogTrigger>
       <DialogContent className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
@@ -51,19 +92,60 @@ export function DialogCategories({
           <DialogHeader>
             <DialogTitle>{type} CATEGORIA</DialogTitle>
           </DialogHeader>
-          <div className="grid gap-4 py-4">
-            <div className="grid grid-cols-4 items-center gap-4">
-              <label htmlFor="description" className="text-right">
-                Descrição:
-              </label>
-              <input
-                id="description"
-                className="col-span-3 border border-grayBase outline-none"
-                value={description}
-                onChange={handleInputChange}
-              />
+
+          {type === 'FILTRAR' ? (
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <label htmlFor="startDate" className="text-right">
+                  Data Inicial:
+                </label>
+                <input
+                  type="date"
+                  id="startDate"
+                  className="col-span-3 border border-grayBase outline-none"
+                  value={startDate}
+                  onChange={handleStartDateChange}
+                  disabled={isDisabledStartDate}
+                />
+              </div>
+              <div className="grid grid-cols-4 items-center gap-4">
+                <label htmlFor="endDate" className="text-right">
+                  Data Final:
+                </label>
+                <input
+                  type="date"
+                  id="endDate"
+                  className="col-span-3 border border-grayBase outline-none"
+                  value={endDate}
+                  onChange={handleEndDateChange}
+                  disabled={isDisabledEndDate}
+                />
+              </div>
+
+              <div className="flex items-center space-x-2">
+                <Switch
+                  id="airplane-mode"
+                  checked={isDisabledEndDate ? false : true}
+                  onCheckedChange={checked => toggleDateFilter(checked)}
+                />
+                <label htmlFor="airplane-mode">Filtro por data</label>
+              </div>
             </div>
-          </div>
+          ) : (
+            <div className="grid gap-4 py-4">
+              <div className="grid grid-cols-4 items-center gap-4">
+                <label htmlFor="description" className="text-right">
+                  Descrição:
+                </label>
+                <input
+                  id="description"
+                  className="col-span-3 border border-grayBase outline-none"
+                  value={description}
+                  onChange={handleInputChange}
+                />
+              </div>
+            </div>
+          )}
 
           <DialogFooter className="flex gap-2 mt-4">
             <DialogClose className="bg-gray-300 p-2 rounded mr-2">
