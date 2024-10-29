c0fff9f541d1c9bd921ad6c677332e20f31094c1
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-10-29 01:49:48 -0300

    refactor: substituir LucideMenu por SheetGeneric no componente Menu

--- src/app/components/homeAuth/Header/Menu/Menu.tsx
+++ src/app/components/homeAuth/Header/Menu/Menu.tsx
@@ -1,12 +1,10 @@
-import { LucideMenu } from 'lucide-react';
 import LogoGeneric from '../../../Common/LogoGeneric/LogoGeneric';
+import { SheetGeneric } from '@/app/components/Common/SheetGeneric/SheetGenetic';
 
 const Menu = () => {
   return (
     <div className="flex space-x-5 items-center">
-      <div className="flex items-center justify-center w-12 h-12 bg-primaryColor rounded-full">
-        <LucideMenu className="text-white w-6 h-6" />
-      </div>
+      <SheetGeneric />
       <LogoGeneric />
     </div>
   );
