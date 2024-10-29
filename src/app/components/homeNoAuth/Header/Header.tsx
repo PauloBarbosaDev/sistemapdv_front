5273983448eeec6968ce4e105581eab26c50a6a0
Paulo Barbosa <paulo_barbosa_9333@outlook.com>
2024-10-29 01:47:34 -0300

    feat: div adicionada para abrir o menu hamburguer na responsividade

--- src/app/components/homeNoAuth/Header/Header.tsx
+++ src/app/components/homeNoAuth/Header/Header.tsx
@@ -1,10 +1,14 @@
 import LogoGeneric from '../../Common/LogoGeneric/LogoGeneric';
+import { SheetGeneric } from '../../Common/SheetGeneric/SheetGenetic';
 import Menu from './Menu/Menu';
 
 const Header = () => {
   return (
     <>
-      <header className="bg-lightBase flex items-center justify-center md:justify-between px-8 min-h-20 shadow-custom sticky top-0 z-20">
+      <header className="bg-lightBase flex items-center justify-center md:justify-between p-4 min-h-20 shadow-custom sticky top-0 z-20">
+        <div className="px-3 md:hidden">
+          <SheetGeneric />
+        </div>
         <LogoGeneric />
         <Menu />
       </header>
